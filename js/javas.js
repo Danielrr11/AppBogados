671833e836c31dc1adf01d98b672e4384fabe8e4
Danielrr11 <romandani24@hotmail.com>
2020-08-07 12:54:17 -0500

    menu statico

--- js/javas.js
+++ js/javas.js
@@ -1,10 +1,21 @@
-  $(function () {
-    $(window).scroll(function(){
-      var winTop = $(window).scrollTop();
-      if(winTop >= 30){
-        $("body").addClass("sticky-header");
-      }else{
-        $("body").removeClass("sticky-header");
-      }
+$(function () {
+  $(window).scroll(function(){
+    var winTop = $(window).scrollTop();
+    if(winTop >= 30){
+      $("body").addClass("sticky-header");
+    }else{
+      $("body").removeClass("sticky-header");
+    }
   })
 })
+
+$(document).ready(function(){
+  var alt = $('.d1').offset().top;
+  $(window).on('scroll', function() {
+    if ($(window).scrollTop() > alt ) {
+      $('.d1').addClass('menu-fixed');
+    }else {
+      $('.d1').removeClass('menu-fixed');
+    }
+  })
+});
